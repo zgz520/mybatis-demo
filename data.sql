54d81849dd27f3373ee92149c2f6eb897d5ee047
USER <zgz612521@163.com>
2020-05-09 15:30:13 +0800

    上传demo     zgz

---
+++ data.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `shop` (
+  `id` int(6) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `scope` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
